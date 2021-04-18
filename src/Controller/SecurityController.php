c2f662cd3a1ec146bc793bf10a2e90752fd48dbd
Vitaly Posadnev <elemabor@gmail.com>
2021-04-19 00:24:57 +0300

    add comment for SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,23 +11,28 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
+     * Обработчик для страницы авторизации
      * @Route("/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // Если пользователь уже авторизован, то редиректим на главную страницу
+        // Тем самым, ограничиваем доступ к странице авторизации
         if ($this->getUser() !== null) {
             return new RedirectResponse($this->generateUrl('app'));
         }
 
         // Получаем последнюю ошибку, если она имеется
         $error = $authenticationUtils->getLastAuthenticationError();
-        // Получаем фамилию пользователя после его авторизации
+        // Получаем фамилию пользователя указанную при авторизации
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        // Вызываем шаблон авторизации и передаем в него последнюю ошибку с фамилией пользователя
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     /**
+     * Обработчик для страницы выхода с аккаунта
      * @Route("/logout", name="app_logout")
      */
     public function logout()
