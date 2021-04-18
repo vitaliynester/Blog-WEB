8b8055e9e3fe68b1d8a71dcf7f6af78430bb6b32
Vitaly Posadnev <elemabor@gmail.com>
2021-04-18 15:10:21 +0300

    update login controller (if user already entered redirect on home)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -14,13 +15,13 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser() !== null) {
+            return new RedirectResponse($this->generateUrl('app'));
+        }
 
-        // get the login error if there is one
+        // Получаем последнюю ошибку, если она имеется
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+        // Получаем фамилию пользователя после его авторизации
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
