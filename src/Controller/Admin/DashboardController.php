b4372316cb74ad20e642aafff3fad9e7c66bfbfc
Vitaly Posadnev <elemabor@gmail.com>
2021-04-26 02:00:27 +0300

    update DashboardController.php

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,10 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use App\Entity\Post;
-use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +18,8 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+        return $this->redirect($routeBuilder->setController(UserCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -29,9 +30,8 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linktoDashboard('Пользователи', 'fa fa-home');
         yield MenuItem::linkToCrud('Посты', 'fas fa-list', Post::class);
         yield MenuItem::linkToCrud('Комментарии', 'fas fa-list', Comment::class);
-        yield MenuItem::linkToCrud('Пользователи', 'fas fa-list', User::class);
     }
 }
