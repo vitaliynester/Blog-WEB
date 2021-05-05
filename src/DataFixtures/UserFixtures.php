f5751224b8eb0b5558ac7b2380e1c1c43e78bd30
Vitaly Posadnev <elemabor@gmail.com>
2021-05-05 13:12:26 +0300

    remove set roles for user in fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -14,6 +14,7 @@ class UserFixtures extends Fixture
 
     /**
      * UserFixtures constructor.
+     *
      * @param UserPasswordEncoderInterface $encoder (сущность для преобразования паролей в правильный вид для БД)
      */
     public function __construct(UserPasswordEncoderInterface $encoder)
@@ -23,24 +24,24 @@ class UserFixtures extends Fixture
 
     /**
      * Метод для загрузки фикстуры (подготовленных данных) в базу данных
+     *
      * @param ObjectManager $manager (менеджер доктрины для взаимодействия с БД)
      */
     public function load(ObjectManager $manager)
     {
         // Создание пользователя с правами администратора
         $adminUser = new User();
-        $adminUser->setEmail("admin@admin.ru");
+        $adminUser->setEmail('admin@admin.ru');
         $adminUser->setPassword($this->passwordEncoder->encodePassword($adminUser, 'hard_admin_passw0rd!'));
-        $adminUser->setLastName("Админин");
-        $adminUser->setFirstName("Админ");
-        $adminUser->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $adminUser->setLastName('Админин');
+        $adminUser->setFirstName('Админ');
+        $adminUser->setRoles(['ROLE_ADMIN']);
         // Создание пользователя с обычными правами
         $basicUser = new User();
-        $basicUser->setEmail("example@mail.ru");
+        $basicUser->setEmail('example@mail.ru');
         $basicUser->setPassword($this->passwordEncoder->encodePassword($basicUser, 'Qwerty123!'));
-        $basicUser->setLastName("Иванов");
-        $basicUser->setFirstName("Петр");
-        $basicUser->setRoles(['ROLE_USER']);
+        $basicUser->setLastName('Иванов');
+        $basicUser->setFirstName('Петр');
         // Добавим пользователей в базу данных
         $manager->persist($adminUser);
         $manager->persist($basicUser);
