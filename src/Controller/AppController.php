5f610dc16a8538f80bb0472c7b9b23c217b6d83b
Vitaly Posadnev <elemabor@gmail.com>
2021-05-06 19:24:41 +0300

    add post repository for home page

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,14 @@ class AppController extends AbstractController
 {
     /**
      * Обработчик для главной страницы
+     *
      * @Route("/", name="app")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'posts' => $postRepository->findBy([], ['dateOfCreation' => 'DESC'], 4),
         ]);
     }
 }
