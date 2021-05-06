77aea22683f4060ddfd3002692b5a6a1cbcb2341
Vitaly Posadnev <elemabor@gmail.com>
2021-05-06 10:24:07 +0300

    create CRUD template for post entity

---
+++ src/Controller/PostController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/post")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="post_index", methods={"GET"})
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/popular", name="post_popular", methods={"GET"})
+     */
+    public function mostViewed(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/discussed", name="post_discussed", methods={"GET"})
+     */
+    public function mostDiscussed(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="post_show", methods={"GET"})
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+}
