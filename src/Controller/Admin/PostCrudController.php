339473c4dab80193cac9ee4676fcf4e894b6bb35
Vitaly Posadnev <elemabor@gmail.com>
2021-05-07 11:48:24 +0300

    add comment for PostCrudController.php

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -16,12 +16,20 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
+    /**
+     * Отображаем только необходимые поля в админ панели
+     *
+     * @param string $pageName (название страницы по которой переходим)
+     * @return iterable (список элементов для отображения сущности поста)
+     */
     public function configureFields(string $pageName): iterable
     {
         return [
+            // Идентификатор поста скрываем при добавлении и редактировании
             IntegerField::new('id')->hideOnForm(),
             IntegerField::new('timeOnRead'),
             TextField::new('title'),
+            // Тело поста скрываем в отображаемом списке
             TextEditorField::new('body')->hideOnIndex(),
             IntegerField::new('countView'),
             DateTimeField::new('dateOfCreation'),
